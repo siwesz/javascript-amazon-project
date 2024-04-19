59d48e57b1241df1822331c040e5c2ff6e23b6aa
sz <siphume9@gmail.com>
2024-04-19 19:08:33 +0200

    Fix Radio Selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -10,6 +10,7 @@
 
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -36,7 +37,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-price">
-      $${(product.priceCents / 100).toFixed(2)}
+     $${formatCurrency(product.priceCents)}
     </div>
 
     <div class="product-quantity-container">
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,13 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+hello();
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM, D'));
 
 let cartSummaryHTML = '';
 
@@ -16,8 +24,7 @@ cart.forEach((cartItem) => {
 
 
   cartSummaryHTML += `
-  <div class="cart-item-container 
-    js-cart-item-container-${matchingProduct.id}">
+<div class="cart-item-container">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -28,10 +35,10 @@ cart.forEach((cartItem) => {
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  ${matchingProduct.name}
+                ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${matchingProduct.priceCents / 100}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -40,8 +47,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" 
-                    data-product-id="${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary">
                     Delete
                   </span>
                 </div>
@@ -54,7 +60,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -67,7 +73,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -80,7 +86,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option- ${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -93,6 +99,7 @@ cart.forEach((cartItem) => {
               </div>
             </div>
           </div>
+
   `;
 });
 
